a293ec451f7c2bf0c8c76e0f97a59e0faf4b37e6
David Pham <a3160100@adelaide.edu.au>
2026-03-22 04:56:08 -0700

    create card header

---
+++ DeadManDraw_GiaBach/Card.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Card {
+
+public:
+    std::string suit;
+    int value;
+
+    Card(std::string suit, int value) :
+
+        suit(suit), value(value)
+
+    {
+    }
+
+    virtual std::string str() const = 0;
+
+};
