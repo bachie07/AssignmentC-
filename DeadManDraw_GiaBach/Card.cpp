334d3b8c4ab426b1028552743ea8c732b0dc8532
David Pham <a3160100@adelaide.edu.au>
2026-03-27 22:43:21 -0700

    update constructor in card.cpp with card type

--- DeadManDraw_GiaBach/Card.cpp
+++ DeadManDraw_GiaBach/Card.cpp
@@ -1,7 +1,7 @@
 #include "Card.h"
 
-Card::Card(std::string suit, int value)  : 
+Card::Card(std::string suit, int value, CardType type)  : 
 
-    suit(suit), value(value)
+    suit(suit), value(value), type(type)
 
 { }
