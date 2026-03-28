05ecba171b4806504cb59907d98fbb5b55b31448
David Pham <a3160100@adelaide.edu.au>
2026-03-27 22:35:08 -0700

    partial implementation of card header

--- DeadManDraw_GiaBach/Card.h
+++ DeadManDraw_GiaBach/Card.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Card {
+
+
+public:
+    std::string suit;
+    int value;
+
+    Card(std::string suit, int value);
+
+    virtual std::string str() const = 0;
+
+
+    virtual void play() = 0;
+
+
+    virtual ~Card() {}
+
+
+
+
+};
