143a2b0f84674d7b96053b6156e7060f99682990
David Pham <a3160100@adelaide.edu.au>
2026-03-28 18:33:53 -0700

    create CardsTypes header file storing all skeleton classes of all card types

---
+++ DeadManDraw_GiaBach/CardTypes.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "Card.h"
+
+
+class ChestCard : Card {
+
+
+};
+
+class ChestCard : Card {
+
+
+};
+
+class KeyCard : Card {
+
+
+};
+
+
+class SwordCard : Card {
+
+
+};
+
+
+class MapCard : Card {
+
+
+};
+
+
+class KrakenCard : Card {
+
+
+};
+
+
+class MermaidCard : Card {
+
+
+};
+
+
+class OracleCard : Card {
+
+
+};
+
+class HookCard : Card {
+
+
+};
+
+
+
+
+
