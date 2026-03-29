b53d4e29b262d74af8424032e918aebf3881c1f3
David Pham <a3160100@adelaide.edu.au>
2026-03-28 20:06:18 -0700

    declare player header class partially

--- DeadManDraw_GiaBach/Player.h
+++ DeadManDraw_GiaBach/Player.h
@@ -1,1 +1,38 @@
 #pragma once
+#include <string>
+#include "Card.h"
+
+class Player {
+
+public:
+
+    std::string name; 
+
+    int score;
+
+    CardCollection _PlayArea;
+
+    CardCollection _bank;
+
+    Player(std::string name);
+
+    
+    bool playCard(Card* card, Game& game);
+
+    bool isBust();
+
+    void moveCardToBank();
+
+    void print();
+
+    const int getScore() const;
+    
+    const std::string getName() const; 
+
+    virtual ~Player() {}
+
+
+    
+
+
+};
